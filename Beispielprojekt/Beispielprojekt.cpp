7aeebe70724b6cc0b5e4ef28c6b2c27415b89dc2
Philipp <piano.philipp@gmail.com>
2018-10-31 17:00:36 +0100

    HInzufügen des zweiten Spielers

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -16,9 +16,9 @@
 #include <algorithm>
 #include <ctime>
 using namespace std;
-// Simulationsgeschwindigkeit
 
 
+//Fenstergröße
 int windowheight =900;
 int windowwidth = 1600;
 
@@ -39,6 +39,8 @@ class GameWindow : public Gosu::Window
 	int pos_p2_x = 1600;
 	int pos_p2_y = 900;
 	const int speed = 5;
+	int leben_p1 = 200;
+	int leben_p2 = 200;
 
 //-------------------------------------------------------
 public:
@@ -66,10 +68,20 @@ public:
 			0.5, 0.5, //Position der "Mitte" in realtiv zu x,y -->0|0 ist oben links 0.5|0.5 ist mitte des Bildes
 			0.5,0.4 //Verhältnis zum Kleiner machen 0.4= 40% des Ursprünglichen
 		);
+		bild.draw_rot(pos_p2_x, pos_p2_y, 0.0,
+			0,//Rotationswinkel
+			0.5, 0.5, //Position der "Mitte" in realtiv zu x,y -->0|0 ist oben links 0.5|0.5 ist mitte des Bildes
+			0.5, 0.4 //Verhältnis zum Kleiner machen 0.4= 40% des Ursprünglichen
+		);
 
 		bild_hg.draw_rot(0, 0, -10, 0, 0, 0); //Hintergrund Zpos -10 als unterstes Bild
 		
-
+		//Lebensanzeige Student
+		Gosu::Graphics::draw_rect(400, 15, 200, 30, Gosu::Color::GRAY, 1);
+		Gosu::Graphics::draw_rect(400, 15, leben_p1, 30, Gosu::Color::GREEN, 2);
+		//Lebensanzeige Dozent
+		Gosu::Graphics::draw_rect(1250, 15, -200, 30, Gosu::Color::GRAY, 1);
+		Gosu::Graphics::draw_rect(1250, 15, -leben_p2, 30, Gosu::Color::GREEN, 2);
 		
 	};
 
@@ -93,28 +105,28 @@ public:
 		{ 
 			close();
 		}
-		//Steuerung Spieler Student
-		if (input().down(Gosu::ButtonName::KB_UP)&&(pos_p1_y>=0))
+		//Steuerung Spieler1 Student
+		if (input().down(Gosu::ButtonName::KB_W)&&(pos_p1_y>=0))
 		{
 			pos_p1_y = pos_p1_y - speed;
 			
 		}
-		if (input().down(Gosu::ButtonName::KB_DOWN) && (pos_p1_y <=windowheight))
+		if (input().down(Gosu::ButtonName::KB_S) && (pos_p1_y <=windowheight))
 		{
 			pos_p1_y = pos_p1_y + speed;
 			
 		}
-		if (input().down(Gosu::ButtonName::KB_LEFT) && (pos_p1_x >= 0))
+		if (input().down(Gosu::ButtonName::KB_A) && (pos_p1_x >= 0))
 		{
 			pos_p1_x = pos_p1_x - speed;
 			
 		}
-		if (input().down(Gosu::ButtonName::KB_RIGHT) && (pos_p1_x <= (windowwidth/2)))
+		if (input().down(Gosu::ButtonName::KB_D) && (pos_p1_x <= (windowwidth/2)))
 		{
 			pos_p1_x = pos_p1_x + speed;
 			
 		}
-		//Steuerung Spieler Dozent
+		//Steuerung Spieler2 Dozent
 		if (input().down(Gosu::ButtonName::KB_UP)&&(pos_p2_y>=0))
 		{
 			pos_p2_y = pos_p2_y - speed;
