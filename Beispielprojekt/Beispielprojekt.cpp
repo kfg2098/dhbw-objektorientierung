0a90839d6c8731f1896c6f6014cb095ff9bfc929
Philipp <piano.philipp@gmail.com>
2018-10-31 15:44:40 +0100

    Steuerung mit Ränder

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -7,8 +7,6 @@
 #include <string>
 #include <iostream>
 
-#include "Planet.h"
-#include "Vektor2d.h"
 
 #include <cstdint>
 #include <memory>
@@ -23,8 +21,6 @@ const double DT = 100.0;
 
 int windowheight =900;
 int windowwidth = 1600;
-int pos_kontrolle_y(int i);
-int pos_kontrolle_x(int i);
 
 //---------------------------------------------------------------------------------
 
@@ -65,7 +61,7 @@ public:
 	
 	void draw() override
 	{
-		bild.draw_rot(pos_p1_x+bild.width()/2, pos_p1_y+bild.height()/2, 0.0,
+		bild.draw_rot(pos_p1_x, pos_p1_y, 0.0,
 			0,//Rotationswinkel
 			0.5, 0.5, //Position der "Mitte" in realtiv zu x,y -->0|0 ist oben links 0.5|0.5 ist mitte des Bildes
 			0.5,0.4 //Verhältnis zum Kleiner machen 0.4= 40% des Ursprünglichen
@@ -117,46 +113,46 @@ public:
 			close();
 		}
 		//Steuerung Spieler Dozent
-		if (input().down(Gosu::ButtonName::KB_UP))
+		if (input().down(Gosu::ButtonName::KB_UP)&&(pos_p1_y>=0))
 		{
 			pos_p1_y = pos_p1_y - speed;
-			pos_kontrolle_y(pos_p1_y);
+			
 		}
-		if (input().down(Gosu::ButtonName::KB_DOWN))
+		if (input().down(Gosu::ButtonName::KB_DOWN) && (pos_p1_y <=windowheight))
 		{
 			pos_p1_y = pos_p1_y + speed;
-			pos_kontrolle_y(pos_p1_y);
+			
 		}
-		if (input().down(Gosu::ButtonName::KB_LEFT))
+		if (input().down(Gosu::ButtonName::KB_LEFT) && (pos_p1_x >= 0))
 		{
 			pos_p1_x = pos_p1_x - speed;
-			pos_kontrolle_x(pos_p1_x);
+			
 		}
-		if (input().down(Gosu::ButtonName::KB_RIGHT))
+		if (input().down(Gosu::ButtonName::KB_RIGHT) && (pos_p1_x <= (windowwidth/2)))
 		{
 			pos_p1_x = pos_p1_x + speed;
-			pos_kontrolle_x(pos_p1_x);
+			
 		}
 		//Steuerung Spieler Student
-		if (input().down(Gosu::ButtonName::KB_UP))
+		if (input().down(Gosu::ButtonName::KB_UP)&&(pos_p2_y>=0))
 		{
 			pos_p2_y = pos_p2_y - speed;
-			pos_kontrolle_y(pos_p2_y);
+			
 		}
-		if (input().down(Gosu::ButtonName::KB_DOWN))
+		if (input().down(Gosu::ButtonName::KB_DOWN)&&(pos_p2_y<=windowheight))
 		{
 			pos_p2_y = pos_p2_y + speed;
-			pos_kontrolle_y(pos_p2_y);
+			
 		}
-		if (input().down(Gosu::ButtonName::KB_LEFT))
+		if (input().down(Gosu::ButtonName::KB_LEFT)&&(pos_p2_x>=(windowwidth/2)))
 		{
 			pos_p2_x = pos_p2_x - speed;
-			pos_kontrolle_x(pos_p2_x);
+			
 		}
-		if (input().down(Gosu::ButtonName::KB_RIGHT))
+		if (input().down(Gosu::ButtonName::KB_RIGHT)&&(pos_p2_x<=1600))
 		{
 			pos_p2_x = pos_p2_x + speed;
-			pos_kontrolle_x(pos_p2_x);
+			
 		}
 		//
 		/*
@@ -181,27 +177,4 @@ int main()
 	
 	GameWindow window;
 	window.show();
-}
-
-//Postitionskontrolle
-int pos_kontrolle_x(int i) {
-	if (i < 0) {
-		i = i + 5;
-		return i;
-	}
-	if (i > 1600) {
-		i = i - 5;
-		return i;
-	}
-}
-int pos_kontrolle_y(int i) {
-	if (i < 0) {
-		i = i + 5;
-		return i;
-	}
-	if (i > 900) {
-		i = i - 5;
-		return i;
-	}
-	else { return i; }
 }
