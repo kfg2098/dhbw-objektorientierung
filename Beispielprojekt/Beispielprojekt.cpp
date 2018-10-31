7dab90884ab925f5a89c62eba2630a06c3575000
kfg2098 <konstantin.gerstner@web.de>
2018-10-31 13:11:14 +0100

    Tastenabfrage eingebaut

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -10,111 +10,124 @@
 #include "Planet.h"
 #include "Vektor2d.h"
 
+#include <cstdint>
+#include <memory>
+#include <cassert>
+#include <cinttypes>
+#include <forward_list>
+#include <algorithm>
+#include <ctime>
+using namespace std;
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
+
+int windowheight = 720;
+int windowwidth = 1280;
 
 class GameWindow : public Gosu::Window
 {
 public:
 	Gosu::Image bild;
 	GameWindow()
-		: Window(800, 600)
-		, bild("rakete.png")
-		// Rakete startet in der Mitte des Bildschirmes
-		, pos(graphics().width() / 2.0, graphics().height() / 2.0)
+		: Window(windowwidth, windowheight)
+		,bild("Kack.png")
+		
 	{
-		set_caption("Gosu Tutorial Game mit Git");
+		set_caption("Keller is coming!");
 
-		// Erzeuge Planeten
-		planets.push_back(Planet({ 200.0, 200.0 }, 0.1, "planet1.png"));
-		planets.push_back(Planet({ 600.0, 200.0 }, 0.1, "planet2.png"));
-		planets.push_back(Planet({ 400.0, 500.0 }, 0.1, "planet3.png"));
 	}
 
 	// wird bis zu 60x pro Sekunde aufgerufen.
 	// Wenn die Grafikkarte oder der Prozessor nicht mehr hinterherkommen,
 	// dann werden `draw` Aufrufe ausgelassen und die Framerate sinkt
+	
+	
 	void draw() override
 	{
-		bild.draw_rot(pos.get_x(), pos.get_y(), 10.0,
-			rot, // Rotationswinkel in Grad
-			0.5, 0.5 // Position der "Mitte"
+		bild.draw_rot(x+bild.width()/2, y+bild.height()/2, 0.0,
+			y-x,//Rotationswinkel
+			0.5, 0.5, //Position der "Mitte" in realtiv zu x,y -->0|0 ist oben links 0.5|0.5 ist mitte des Bildes
+			0.5,0.4 //Verhältnis zum Kleiner machen 0.4= 40% des Ursprünglichen
 		);
 
-		auto g2 = (gravity * 1000000000000.0).log();
+		graphics().draw_rect(0, 0, windowwidth, windowheight, Gosu::Color::WHITE, -1); //HG
+		
+		/*
+		graphics().draw_line(				//Linie zeichnen funktion
+			10, 20, Gosu::Color::RED,		//Startpunkt der Linie mit Farbe
+			200, 100, Gosu::Color::GREEN,	//Endpunkt der Linie mit Farbe--> Farbverlauf
+			0.0
+		);
+		*/
 
-		Vektor2d rose(50.0, 50.0);
-		auto g = rose - g2;
-		auto s = rose + speed * 1000.0;
+		//graphics().draw_triangle(30,x,Gosu::Color::GREEN,y,100, Gosu::Color::RED,x,y, Gosu::Color::BLUE,0.0);
+		//graphics().draw_quad(100, 100, Gosu::Color::BLUE,100, 200, Gosu::Color::YELLOW,300, 300, Gosu::Color::CYAN,300,400,Gosu::Color::RED,0.0);
+		//graphics().draw_rect(400,300,y,y,Gosu::Color::CYAN,0.0);
+		
 
-		graphics().draw_line(pos.get_x(), pos.get_y(), Gosu::Color::GREEN, input().mouse_x(), input().mouse_y(), Gosu::Color::GREEN, -10.0);
-		graphics().draw_line(rose.get_x(), rose.get_y(), Gosu::Color::RED, g.get_x(), g.get_y(), Gosu::Color::RED, 10.0);
-		graphics().draw_line(rose.get_x(), rose.get_y(), Gosu::Color::BLUE, s.get_x(), s.get_y(), Gosu::Color::BLUE, 10.0);
+		Gosu::Graphics::draw_line(0, 20, Gosu::Color::RED,1280, 20, Gosu::Color::GREEN,0.0);
 
-		for (auto planet : planets) {
-			planet.draw();
+		if (space)
+		{
+			graphics().draw_line(				//Linie zeichnen funktion
+			 60, 80, Gosu::Color::BLUE,		//Startpunkt der Linie mit Farbe
+			200, 100, Gosu::Color::YELLOW,	//Endpunkt der Linie mit Farbe--> Farbverlauf
+			0.0
+			);
 		}
-	}
 
-	double rot = 0.0;
-	Vektor2d pos, speed, gravity;
-	double accel = 0.0;
-	std::vector<Planet> planets;
+		if (hoch)
+		{
+			graphics().draw_rect(200, 200, 200, 200, Gosu::Color::BLUE,-0.5);
+		}
+
+		if (runter)
+		{
+			graphics().draw_rect(200, 200, 200, 200, Gosu::Color::GREEN, 0.0);
+		}
+	};
+
+	
+
+	double x = 0;
+	double y = 0;
+	bool space = false;
+	bool hoch = false;
+	bool runter = false;
+	bool rechts = false;
+	bool links = false;
+
 
 	// Wird 60x pro Sekunde aufgerufen
 	void update() override
 	{
-		// Geschwindigkeit führt zu Positionsänderung
-		pos += speed * DT;
+		x = input().mouse_x();
+		y = input().mouse_y();
 
-		// Beschleunigung während "W" gedrückt
-		if (input().down(Gosu::KB_W)) {
-			accel += 0.00001;
-		}
-		else {
-			accel -= 0.00002;
+		//Tastenabfrage
+		if (input().down(Gosu::ButtonName::KB_ESCAPE))//schließt, wenn ESC gedrückt wurde
+		{ 
+			close();
 		}
 
-		// Maximale Beschleunigung
-		accel = Gosu::clamp(accel, 0.0, 0.0001);
-
-		// Geschwindigkeit wird in Flugrichtung geändert
-		speed += Vektor2d::from_angle(rot, accel);
-
-
-		gravity = Vektor2d();
-
-		// Planeten ziehen Raumschiff an
-		for (auto planet : planets) {
-
-			// Entfernung im Quadrat
-			double dist = (pos - planet.pos).length_squared();
-
-			// Winkel des Vektors vom Raumschiff zum Planet
-			double angle = pos.angle(planet.pos);
-
-			// Anziehungskraft
-			double pull = planet.mass / dist;
-
-			// Kollision
-			if (dist < 1000) {
-				speed = Vektor2d::from_angle(angle, pull);
-				break;
-			}
-			gravity += Vektor2d::from_angle(angle, pull);
-		}
-
-		speed += gravity;
-
-		// Raumschiff in Richtung Mauszeiger drehen
-		double angle = pos.angle({ input().mouse_x(), input().mouse_y() });
-		rot -= Gosu::angle_diff(angle, rot) / 36.0;
-	}
+		space = input().down(Gosu::KB_SPACE); //Abfrage ob Space gedrückt ist, wenn ja siehe draw Methode
+		hoch = input().down(Gosu::KB_UP);
+		runter = input().down(Gosu::KB_DOWN);
+		rechts = input().down(Gosu::KB_RIGHT);
+		links = input().down(Gosu::KB_LEFT);
+		
+	};
+	
 };
+
+
 
 // C++ Hauptprogramm
 int main()
 {
+	
+	
+	
 	GameWindow window;
 	window.show();
 }
