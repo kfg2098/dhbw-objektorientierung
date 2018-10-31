769248306442f425e3315b7e68e1372fe213b8f0
kfg2098 <konstantin.gerstner@web.de>
2018-10-31 15:14:28 +0100

    Bild Hintergrund

--- Beispielprojekt/Beispielprojekt.cpp
+++ Beispielprojekt/Beispielprojekt.cpp
@@ -21,8 +21,8 @@ using namespace std;
 // Simulationsgeschwindigkeit
 const double DT = 100.0;
 
-int windowheight = 720;
-int windowwidth = 1280;
+const int windowheight = 900;
+const int windowwidth = 1600;
 
 class GameWindow : public Gosu::Window
 {
@@ -30,7 +30,7 @@ public:
 	Gosu::Image bild;
 	GameWindow()
 		: Window(windowwidth, windowheight)
-		,bild("Kack.png")
+		,bild("Hintergrund.png") 
 		
 	{
 		set_caption("Keller is coming!");
@@ -44,28 +44,11 @@ public:
 	
 	void draw() override
 	{
-		bild.draw_rot(x+bild.width()/2, y+bild.height()/2, 0.0,
-			y-x,//Rotationswinkel
-			0.5, 0.5, //Position der "Mitte" in realtiv zu x,y -->0|0 ist oben links 0.5|0.5 ist mitte des Bildes
-			0.5,0.4 //Verhältnis zum Kleiner machen 0.4= 40% des Ursprünglichen
-		);
+		bild.draw_rot(0, 0,	0.0,	//X y z Position
+			0,					//Rotationswinkel in Grad
+			0, 0);			//Position der Mitte reativ zu x,y
 
-		graphics().draw_rect(0, 0, windowwidth, windowheight, Gosu::Color::WHITE, -1); //HG
 		
-		/*
-		graphics().draw_line(				//Linie zeichnen funktion
-			10, 20, Gosu::Color::RED,		//Startpunkt der Linie mit Farbe
-			200, 100, Gosu::Color::GREEN,	//Endpunkt der Linie mit Farbe--> Farbverlauf
-			0.0
-		);
-		*/
-
-		//graphics().draw_triangle(30,x,Gosu::Color::GREEN,y,100, Gosu::Color::RED,x,y, Gosu::Color::BLUE,0.0);
-		//graphics().draw_quad(100, 100, Gosu::Color::BLUE,100, 200, Gosu::Color::YELLOW,300, 300, Gosu::Color::CYAN,300,400,Gosu::Color::RED,0.0);
-		//graphics().draw_rect(400,300,y,y,Gosu::Color::CYAN,0.0);
-		
-
-		Gosu::Graphics::draw_line(0, 20, Gosu::Color::RED,1280, 20, Gosu::Color::GREEN,0.0);
 
 		if (space)
 		{
@@ -127,7 +110,7 @@ int main()
 {
 	
 	
-	cout << "Hallo Konstantin" << endl;
+	
 	GameWindow window;
 	window.show();
 }
